d9c1189933613ad6239cb565f4f08535442eeb6e
Jonnua <jonarrahmani4@gmail.com>
2025-02-01 01:26:30 +0100

    Order Header and Details Models added

---
+++ Models/OrderHeader.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace NojeMenu_API.Models
+{
+    public class OrderHeader
+    {
+        [Key]
+        public int OrderHeaderId { get; set; }
+        [Required]
+        public string PickupName { get; set; }
+        [Required]
+        public string PickupPhoneNumber { get; set; }
+        [Required]
+        public string PickupEmail { get; set; }
+        
+
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]  //only if registered, can order
+        public ApplicationUser User { get; set; }
+        public double OrderTotal { get; set; }
+
+
+        public DateTime OrderDate { get; set; }
+        public string StripePaymentIntentId { get; set; } //id of the payment that a user makes
+        public string Status { get; set; }
+        public int TotalItems { get; set; }
+
+        public IEnumerable<OrderDetails> OrderDetails { get; set; }
+
+    }
+}
