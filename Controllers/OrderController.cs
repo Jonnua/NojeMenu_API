34b0d17a94dab9684daf92811072114fcf8e8296
Jonnua <jonarrahmani4@gmail.com>
2025-02-01 02:35:48 +0100

    Updated Order Details

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -132,5 +132,68 @@ namespace NojeMenu_API.Controllers
             }
             return _response;
         }
-    }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<ApiResponse>> UpdateOrderHeader(int id, [FromBody] OrderHeaderUpdateDTO orderHeaderUpdateDTO)
+        {
+            try
+            {
+                if(orderHeaderUpdateDTO == null || id != orderHeaderUpdateDTO.OrderHeaderId)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode=HttpStatusCode.BadRequest;
+                    return BadRequest();
+                }
+                OrderHeader orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.OrderHeaderId == id);
+
+                if (orderFromDb == null) 
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode=HttpStatusCode.BadRequest;
+                    return BadRequest();
+                }
+
+                if (!string.IsNullOrEmpty(orderHeaderUpdateDTO.PickupName))
+                {
+                    orderFromDb.PickupName = orderHeaderUpdateDTO.PickupName;
+                }
+                if (!string.IsNullOrEmpty(orderHeaderUpdateDTO.PickupPhoneNumber))
+                {
+                    orderFromDb.PickupPhoneNumber = orderHeaderUpdateDTO.PickupPhoneNumber;
+                }
+                if (!string.IsNullOrEmpty(orderHeaderUpdateDTO.PickupEmail))
+                {
+                    orderFromDb.PickupEmail = orderHeaderUpdateDTO.PickupEmail;
+                }
+                if (!string.IsNullOrEmpty(orderHeaderUpdateDTO.Status))
+                {
+                    orderFromDb.Status = orderHeaderUpdateDTO.Status;
+                }
+                if (!string.IsNullOrEmpty(orderHeaderUpdateDTO.StripePaymentIntentId))
+                {
+                    orderFromDb.StripePaymentIntentId = orderHeaderUpdateDTO.StripePaymentIntentId;
+                }
+                _db.SaveChanges();
+                _response.StatusCode = HttpStatusCode.NoContent;
+                _response.IsSuccess = true;
+                return Ok(_response);
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                    = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+
+
+
+        }
 }
--- Models/Dto/OrderHeaderUpdateDTO.cs
+++ Models/Dto/OrderHeaderUpdateDTO.cs
@@ -6,7 +6,6 @@
         public string PickupName { get; set; }
         public string PickupPhoneNumber { get; set; }
         public string PickupEmail { get; set; }
-        public DateTime OrderDate { get; set; }
         public string StripePaymentIntentId { get; set; }
         public string Status { get; set; }
     }
