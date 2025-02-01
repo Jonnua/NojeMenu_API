3046adca1b5062a94c2098dbdc8c97c2e1c510f5
Jonnua <jonarrahmani4@gmail.com>
2025-02-01 01:33:43 +0100

    Order DTOs created

---
+++ Models/Dto/OrderHeaderCreateDTO.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace NojeMenu_API.Models.Dto
+{
+    public class OrderHeaderCreateDTO
+    {
+        [Required]
+        public string PickupName { get; set; }
+        [Required]
+        public string PickupPhoneNumber { get; set; }
+        [Required]
+        public string PickupEmail { get; set; }
+        [Required]
+        public string PickupAddress { get; set; }
+
+        public string ApplicationUserId { get; set; }
+        public double OrderTotal { get; set; }
+
+
+        public string StripePaymentIntentId { get; set; }
+        public string Status { get; set; }
+        public int TotalItems { get; set; }
+
+
+        public IEnumerable<OrderDetailsCreateDTO> OrderDetailsDTO { get; set; }
+    }
+}
---
+++ Models/Dto/OrderHeaderUpdateDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace NojeMenu_API.Models.Dto
+{
+    public class OrderHeaderUpdateDTO
+    {
+        public int OrderHeaderId { get; set; }
+        public string PickupName { get; set; }
+        public string PickupPhoneNumber { get; set; }
+        public string PickupEmail { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string StripePaymentIntentId { get; set; }
+        public string Status { get; set; }
+    }
+}
