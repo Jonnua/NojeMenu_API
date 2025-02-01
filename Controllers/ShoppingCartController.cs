d4889670e70f66e690ef88d581af85581fefa81a
Jonnua <jonarrahmani4@gmail.com>
2025-02-01 03:27:01 +0100

    Made some little changes at ShoppingCartController

--- Controllers/ShoppingCartController.cs
+++ Controllers/ShoppingCartController.cs
@@ -25,17 +25,18 @@ namespace NojeMenu_API.Controllers
         {
             try
             {
+                ShoppingCart shoppingCart;
                 if (string.IsNullOrEmpty(userId))
                 {
-                    _response.IsSuccess = false;
-                    _response.StatusCode = HttpStatusCode.BadRequest;
-                    return BadRequest(_response);
+                    shoppingCart = new();
+                }
+                else
+                {
+                     shoppingCart = _db.ShoppingCarts
+                    .Include(u => u.CartItems).ThenInclude(u => u.MenuItem)
+                    .FirstOrDefault(u => u.UserId==userId);
                 }
 
-                ShoppingCart shoppingCart = _db.ShoppingCarts
-                    .Include(u => u.CartItems).ThenInclude(u=>u.MenuItem)
-                    .FirstOrDefault(u=>u.UserId==userId);
-               
                 if(shoppingCart.CartItems != null && shoppingCart.CartItems.Count > 0)
                 {
                     shoppingCart.CartTotal = shoppingCart.CartItems.Sum(u => u.Quantity * u.MenuItem.Price);
