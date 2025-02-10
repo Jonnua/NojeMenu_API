c052f28326ada474e52097b794885cdc7670e374
Jonnua <jonarrahmani4@gmail.com>
2025-02-10 15:44:36 +0100

    Call slice to fetch data and displayed loading text

--- NojeMenu_UI/noje/src/Components/Page/MenuItems/MenuItemList.tsx
+++ NojeMenu_UI/noje/src/Components/Page/MenuItems/MenuItemList.tsx
@@ -12,24 +12,21 @@ function MenuItemList() {
  const dispatch = useDispatch();
  const { data, isLoading } = useGetMenuItemsQuery(null);
 
- /**  useEffect(() => {
-   if(!isLoading){
-    dispatch(setMenuItem(data.result));
-   }    
-  }, [isLoading]);
-*/
+  useEffect(()=>{
+    if(!isLoading){
+      dispatch(setMenuItem(data.result));
+    }
+  }, []);
 
-useEffect(() => {
-  if (!isLoading && data?.result) {
-    dispatch(setMenuItem(data.result));
+  if(isLoading){
+    return <div>Loading...</div>
   }
-}, [isLoading, data, dispatch]); // ✅ Now runs when `data` updates
-
-
+  
     
    return (
     <div className="container row">
-      {data.result.map((menuItem: menuItemModel, index: number) => (
+      {data.result.length > 0 &&
+      data.result.map((menuItem: menuItemModel, index: number) => (
         <MenuItemCard menuItem={menuItem} key={index} />
       ))}
     </div>
