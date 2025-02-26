256425c40bf73729048afcd488c4bed717fad937
edd1ekrs <edi04.ek@gmail.com>
2025-02-26 17:54:59 +0100

    Ni ndryshim tek Payment API

---
+++ NojeMenu_UI/noje/src/Apis/paymentApi.ts
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const paymentApi = createApi({
+  reducerPath: "paymentApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://nojemenuapi.azurewebsites.net/api/",
+  }),
+  endpoints: (builder) => ({
+    initiatePayment: builder.mutation({
+      query: (userId) => ({
+        url: "menuitem",
+        method: "POST",
+        params:{
+            userId:userId
+        }
+      }),
+ 
+    }),
+   
+  }),
+});
+
+export const { useInitiatePaymentMutation } = paymentApi;
+export default paymentApi;
