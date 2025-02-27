83ab1f082c4949c80d9ac49393c8482ebf30aee5
Jonnua <jonarrahmani4@gmail.com>
2025-02-27 18:07:26 +0100

    Local interface added and OrderSummary UI

--- NojeMenu_UI/noje/src/Pages/Payment.tsx
+++ NojeMenu_UI/noje/src/Pages/Payment.tsx
@@ -3,14 +3,14 @@ import { useLocation } from 'react-router';
 import {Elements} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
 import  PaymentForm  from '../Components/Page/Payment/PaymentForm.tsx';
+import OrderSummary from '../Components/Page/Order/OrderSummary.tsx';
 
 function Payment() {
   const {
     state: { apiResult, userInput },
   } = useLocation();
 
-
-  const stripePromise = loadStripe('pk_test_51Qwn50BXjHHynCsKWGfQd6LFlWM3epFyZTEaBCHY6XFqZdt7OJU9IWIqceMKEY7xGXUlIISmDvRI79QDotX4AE2z00tRrjQTA7');
+  const stripePromise = loadStripe('pk_test_51QnVlpFKgnPlJsmnRsgdu2Ejgncfnb6rvDG8Do5fTwmKrbkiWlMW7Ej17Pd2bu1X1m83FOyoeBwE4BddQyvepj1d00phsv2QhM');
   const options = {
     // passing the client secret obtained from the server
     clientSecret: apiResult.clientSecret,
@@ -20,7 +20,9 @@ function Payment() {
     <Elements stripe={stripePromise} options={options}>
       <div className= " container m-5 p-5 ">
         <div className="row">
-          <div className="col-md-5">Order SUmmary</div>
+          <div className="col-md-5">
+            <OrderSummary data={apiResult} userInput={userInput}/>
+            </div>
           <div className="col-md-5">
     < PaymentForm/>
           </div>
