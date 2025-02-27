c8863d3bdee5c99ea1768f802e48e88a22fdf863
Jonnua <jonarrahmani4@gmail.com>
2025-02-27 19:03:59 +0100

    Stripe Processing

--- NojeMenu_UI/noje/src/Helper/taostNotify.ts
+++ NojeMenu_UI/noje/src/Helper/taostNotify.ts
@@ -1,18 +1,17 @@
-import { toast, TypeOptions } from 'react-toastify';
+import { toast, TypeOptions } from "react-toastify";
 
-
-const toastNotify = (message: string, type : TypeOptions="success") => {
-    toast(message, {
-        type: type,
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: true,
-        progress: undefined,
-        theme: "dark",
-    });
+const toastNotify = (message: string, type: TypeOptions = "success") => {
+  toast(message, {
+    type: type,
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
 };
 
 export default toastNotify;
