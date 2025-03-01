b07d274fee3db0ec393bc88e6bd45fd187a4fd11
edd1ekrs <edi04.ek@gmail.com>
2025-03-01 23:19:20 +0100

    Banner added

--- NojeMenu_UI/noje/src/Pages/Home.tsx
+++ NojeMenu_UI/noje/src/Pages/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import MenuItemList from '../Components/Page/MenuItems/MenuItemList.tsx';
+import Banner from '../Components/Page/Common/Banner.tsx';
 
 
 
 function Home() {
   return (
     <div>
+      <Banner/>
   <div className="container p-2">
     <MenuItemList />
     </div>
