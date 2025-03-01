09f0e337e7fab203711c05359cb32321aa5203dc
Jonnua <jonarrahmani4@gmail.com>
2025-03-01 16:13:20 +0100

    buttons for next status

--- NojeMenu_UI/noje/src/Components/Page/Order/OrderSummary.tsx
+++ NojeMenu_UI/noje/src/Components/Page/Order/OrderSummary.tsx
@@ -3,10 +3,24 @@ import { cartItemModel } from "../../../Interfaces";
 import { orderSummaryProps } from "./orderSummaryProps";
 import getStatusColor from "../../../Helper/getStatusColor.ts";
 import { useNavigate } from "react-router-dom";
+import { SD_Roles, SD_Status } from "../../../Utility/SD.ts";
+import { RootState } from "../../../Storage/Redux/store.ts";
+import { useSelector } from "react-redux";
 
 function OrderSummary({ data, userInput }: orderSummaryProps) {
   const badgeTypeColor = getStatusColor(data.status!);
   const navigate = useNavigate();
+  const userData = useSelector((state: RootState) => state.userAuthStore);
+  const nextStatus: any =
+    data.status! === SD_Status.CONFIRMED
+      ? { color: "info", value: SD_Status.BEING_COOKED }
+      : data.status! === SD_Status.BEING_COOKED
+      ? { color: "warning", value: SD_Status.READY_FOR_PICKUP }
+      : data.status! === SD_Status.READY_FOR_PICKUP && {
+          color: "success",
+          value: SD_Status.COMPLETED,
+        };
+
   return (
     <div>
       {" "}
@@ -52,6 +66,15 @@ function OrderSummary({ data, userInput }: orderSummaryProps) {
         <button className="btn btn-secondary" onClick={() => navigate(-1)}>
           Back to Orders
         </button>
+        {userData.role == SD_Roles.ADMIN && (
+          <div className="d-flex">
+            <button className="btn btn-danger mx-2"> Cancel</button>
+
+            <button className={`btn btn-${nextStatus.color}`}>
+              {nextStatus.value}
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
